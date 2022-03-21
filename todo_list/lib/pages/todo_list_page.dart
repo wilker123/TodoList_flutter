784ccb9b73b3f2a0ccf5ab99d5c306852f29444b
wilker123 <josewilkerlucena123@gmail.com>
2022-03-21 11:19:52 -0300

    Created listView and refactoring project

--- todo_list/lib/pages/todo_list_page.dart
+++ todo_list/lib/pages/todo_list_page.dart
@@ -9,30 +9,76 @@ class TodoListPage extends StatelessWidget {
       body: Center(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Row(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
             children: [
-              const Expanded(
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: "Adicione uma tarefa",
-                    hintText: "Ex. Estudar flutter",
+              Row(
+                children: [
+                  const Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: "Adicione uma tarefa",
+                        hintText: "Ex. Estudar flutter",
+                      ),
+                    ),
                   ),
-                ),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      primary: const Color(0xff00d7f3),
+                      padding: const EdgeInsets.all(14),
+                    ),
+                    child: const Icon(
+                      Icons.add,
+                      size: 30,
+                    ),
+                  ),
+                ],
               ),
               const SizedBox(
-                width: 8,
+                height: 16,
               ),
-              ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  primary: Color(0xff00d7f3),
-                  padding: EdgeInsets.all(14),
-                ),
-                child: const Icon(
-                  Icons.add,
-                  size: 30,
-                ),
+              ListView(
+                shrinkWrap: true,
+                children: [
+                  Container(
+                    height: 50,
+                    color: Colors.red,
+                  ),
+                  Container(
+                    height: 50,
+                    color: Colors.green,
+                  ),
+                  Container(
+                    height: 50,
+                    color: Colors.yellow,
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Row(
+                children: [
+                  const Expanded(
+                    child: Text("Você possui 0 tarefas pendentes"),
+                  ),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text("Limpar tudo"),
+                    style: ElevatedButton.styleFrom(
+                      primary: const Color(0xff00d7f3),
+                      padding: const EdgeInsets.all(14),
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
